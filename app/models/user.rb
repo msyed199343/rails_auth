a6ab0ecbae4b84c913c65d9be0d0ec0630a0eb2c
Syed Mohiuddin <msyed199343@gmail.com>
2020-01-24 15:19:28 -0600

    session tokens

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: {message: 'Password can\'t 
+        be blank'}
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+        def self.find_by_credentials(username, password)
+           user =  user.find_by(username: username)
+            return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+            nil
+        end
+
+        def self.generate_session_token
+            SecureRandom::urlsafe_base64(16)
+        end
+
+        def reset_session_token!
+           session_token =  user.generate_session_token
+           session_token.save!
+           self.session_token
+        end
+end
